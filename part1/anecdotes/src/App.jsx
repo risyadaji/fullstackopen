707ad05a8f35e18d769e197a201c48c645e2b82b
Risyad Aji <ris.a.dharma@gmail.com>
2024-08-01 21:58:54 +0700

    anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Header = ({ title }) => <h1>{title}</h1>;
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
+const randomNumber = (max) => Math.floor(Math.random() * max);
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const totalAnecdotes = anecdotes.length;
+
+  const [votes, setVotes] = useState(Array(totalAnecdotes).fill(0));
+  const [selected, setSelected] = useState(randomNumber(totalAnecdotes));
+  const [maxVoteIndex, setMaxVoteIndex] = useState(0);
+
+  const onNextClick = () => setSelected(randomNumber(totalAnecdotes));
+  const onVoteClick = () => {
+    const newVote = [...votes]; // avoid mutate array
+    newVote[selected]++;
+    setVotes(newVote);
+
+    // update maxVote and re-render anecdote most votes
+    if (newVote[selected] > newVote[maxVoteIndex]) {
+      setMaxVoteIndex(selected);
+    }
+  };
+
+  return (
+    <>
+      <Header title="Anecdote of the day" />
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <Button text="vote" onClick={onVoteClick} />
+      <Button text="next anecdote" onClick={onNextClick} />
+
+      <Header title="Anecdote with most votes" />
+      <div>{anecdotes[maxVoteIndex]}</div>
+      <div>has {votes[maxVoteIndex]} votes</div>
+    </>
+  );
+};
+
+export default App;
