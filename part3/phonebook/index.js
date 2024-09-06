7dcd1098eaa2a910678e51a78214d6c25cd14549
Risyad Aji <ris.a.dharma@gmail.com>
2024-09-06 21:15:19 +0700

    exercise 3.7 - 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', (req) => {
+  let { body } = req
+  if (body.hasOwnProperty('number')) {
+    body['number'] = '******' // sanitize
+  }
+
+  return JSON.stringify(body)
+})
+
 app.use(express.json())
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
