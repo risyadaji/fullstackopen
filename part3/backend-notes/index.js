272b216e55864e9f628817940a050f51a2462dfb
Risyad Aji <ris.a.dharma@gmail.com>
2024-09-08 20:44:32 +0700

    chore: integrate put and error handling

--- part3/backend-notes/index.js
+++ part3/backend-notes/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const cors = require('cors')
 // Setup
 const app = express()
+
+// Models
 const Note = require('./models/notes')
 
 const requestLogger = (req, res, next) => {
@@ -14,14 +16,24 @@ const requestLogger = (req, res, next) => {
   next()
 }
 
+// Exception Handler middlewares
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
+}
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
 }
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-app.use(requestLogger())
+app.use(requestLogger)
 
 // Routes
 app.get('/', (req, res) => {
@@ -32,8 +44,16 @@ app.get('/api/notes', (req, res) => {
   Note.find({}).then((notes) => res.json(notes))
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then((note) => res.json(note))
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -51,14 +71,27 @@ app.post('/api/notes', (req, res) => {
   note.save().then((savedNote) => res.json(savedNote))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = req.params.id
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
 
-  notes = notes.filter((note) => note.id !== id)
-  res.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => res.json(updatedNote))
+    .catch((error) => next(error))
 })
 
-app.use(unknownEndpoint())
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((_) => res.status(204).end())
+    .catch((error) => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
